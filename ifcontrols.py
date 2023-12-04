7b7008afebb035390005d1a6c3eaae67ce6c6497
Aleyna Cankat <aleynacankat0@gmail.com>
2023-12-04 22:58:39 +0300

    ifcontrols

---
+++ ifcontrols.py
@@ -0,0 +1,48 @@
+x = 5
+y = 2
+
+x > y
+y < x
+
+y = 5
+
+x >= y
+x <= y
+
+print(1>2 or 3<2)
+
+print(10 in [10,20,30])
+
+print(5 not in [10,20,30])
+
+mydictionary = {"a":10, "b": 20, "c":30}
+
+print(10 in mydictionary.values())
+
+"b" in mydictionary.keys()
+
+mySuperHero = "Batman"
+
+mySuperHero == "Batman"
+
+if mySuperHero == "Batman":
+    print("batmann")
+    print("aleyna cankat")
+
+if mySuperHero == "Superman":
+    print("superman")
+elif mySuperHero == "Batman":
+    print("batman")
+
+myage = 18
+
+if myage <= 18:
+    print("age = 18")
+elif myage > 18 and myage <= 30:
+    print("18 - age - 30")
+elif myage > 30 and myage <= 40:
+    print("30 - age - 40")
+else: print("age > 40")
+
+mySuperHero1 = input("enter superhero: ")
+
